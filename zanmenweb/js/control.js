277561d8e9d623f947517410704187617ace344d
PhamVanMem <mem.pv3396@arena-multimedia.edu.vn>
2025-04-07 20:13:16 +0700

    Add files via upload

---
+++ zanmenweb/js/control.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: [" Web Designer.", " Code Developer."," Apps Designer"," Film Maker"],
+       	 	typeSpeed:29,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+$('.counter').countUp();
+<script> 
+
+  AOS.init(); 
+
+</script> 
